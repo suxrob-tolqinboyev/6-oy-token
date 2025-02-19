4063a452b9f7f0e30a68f550f777a347e0e3a775
suxrob-tolqinboyev <tolqinboyevsuxrob053@gmail.com>
2025-02-20 01:02:41 +0500

    add

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router";
 import Home from "../components/home/index";
 import Admin from "../components/admin/index";
-// import Login from "../components/login";
-import Logina from "../components/login/login.jsx";
+
 import Layout from "../components/layout/index.jsx";
-import Product from "../components/Product/index.jsx";
+
 import Cart from "../components/cartProduct/index.jsx";
 import About from "../pages/About/index.jsx";
+import Login from "../components/login/login.jsx";
 
 const Router = () => {
   return (
@@ -19,7 +19,7 @@ const Router = () => {
           <Route path="/cart" element={<Cart />}></Route>
         </Route>
         <Route path="/admin" element={<Admin />}></Route>
-        <Route path="/logina" element={<Logina />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </>
   );
